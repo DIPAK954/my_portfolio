1a9bdb4533308663a99a0dd99494e45863b37a48
unknown <dipakdhrangiwork954@gmail.com>
2024-06-12 01:09:48 +0530

    version 0.2

--- src/Component/Home.jsx
+++ src/Component/Home.jsx
@@ -1,7 +1,7 @@
 
 const Home =()=>{
     return <div id="home" className="home">
-       <p>Hi,I am Dipak</p> 
+       <p id="my_name">Hi,I am Dipak</p> 
        <p>A Frontend Developer</p>     
     </div>
 }
--- src/Component/Projects.jsx
+++ src/Component/Projects.jsx
@@ -1,14 +1,39 @@
-const Projects =()=>{
-    return  <div className="Projects" id="id_projects"> 
-    <p id="Projects">Projects</p>
-    <div className="Projects_main">
-        <div className="projects_inner"><p className="projects_name">Weather App</p></div>
-        <div className="projects_inner"><p className="projects_name">E-Commerce App</p></div>
-        <div className="projects_inner"><p className="projects_name"> Blog App</p></div>
-      
-      
+import { FaGithub } from "react-icons/fa";
+const Projects = () => {
+  return (
+    <div className="Projects" id="id_projects">
+      <p id="Projects">Projects</p>
+      <div className="Projects_main">
+        <div className="projects_inner">
+          <p className="projects_name">
+            Weather App
+            <br></br>
+            <a href="https://github.com/DIPAK954/PRODIGY_WD_05" target="_blank">
+              <FaGithub />
+            </a>
+          </p>
+        </div>
+        <div className="projects_inner">
+          <p className="projects_name">
+            E-commerce App
+            <br></br>
+            <a href="https://github.com/DIPAK954/E-commerce" target="_blank">
+              <FaGithub></FaGithub>
+            </a>
+          </p>
+        </div>
+        <div className="projects_inner">
+          <p className="projects_name">
+            Portfolio
+            <br></br>
+            <a href="https://github.com/DIPAK954/my_portfolio" target="_blank">
+              <FaGithub></FaGithub>
+            </a>
+          </p>
+        </div>
+      </div>
     </div>
-  </div>;
-}
+  );
+};
 
 export default Projects;
