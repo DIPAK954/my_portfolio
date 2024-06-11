87b7b03814c5cf19c88d341b98c6327b1ca6c7b6
unknown <dipakdhrangiwork954@gmail.com>
2024-06-11 23:58:21 +0530

    version 0.1

--- src/Component/Projects.jsx
+++ src/Component/Projects.jsx
@@ -2,7 +2,7 @@ const Projects =()=>{
     return  <div className="Projects" id="id_projects"> 
     <p id="Projects">Projects</p>
     <div className="Projects_main">
-        <div className="projects_inner"><p className="projects_name">Todo App</p></div>
+        <div className="projects_inner"><p className="projects_name">Weather App</p></div>
         <div className="projects_inner"><p className="projects_name">E-Commerce App</p></div>
         <div className="projects_inner"><p className="projects_name"> Blog App</p></div>
       
